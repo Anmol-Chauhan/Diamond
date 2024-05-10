541fb6a090fe324da9845c34c5d26e70bffdf632
anmol chauhan <anmol.chauhan207@webkul.in>
2024-05-10 12:12:46 +0530

    dummy changes

--- database/migrations/2019_06_11_174319_create_mpauthorizenet_cart_table.php
+++ database/migrations/2019_06_11_174319_create_mpauthorizenet_cart_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateMpauthorizenetCartTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('mpauthorizenet_cart', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreign('cart_id')->references('id')->on('cart')->onDelete('cascade');
+            $table->integer('cart_id')->unsigned();
+            $table->json('mpauthorizenet_token');
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('mpauthorizenet_cart');
     }
 }
